6889cbe993094ce310983b527ba62e4695456573
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-12-03 23:18:03 +0100

    Add com.android.test and todo for next steps.

--- src/main/groovy/com/vanniktech/code/quality/tools/CodeQualityToolsPlugin.groovy
+++ src/main/groovy/com/vanniktech/code/quality/tools/CodeQualityToolsPlugin.groovy
@@ -355,21 +355,28 @@ class CodeQualityToolsPlugin implements Plugin<Project> {
   }
 
   private static boolean isKotlinProject(final Project project) {
-    final boolean isJava = project.plugins.hasPlugin('kotlin')
-    final boolean isJavaLibrary = project.plugins.hasPlugin('kotlin-android')
-    return isJava || isJavaLibrary
+    final boolean isKotlin = project.plugins.hasPlugin('kotlin')
+    final boolean isKotlinAndroid = project.plugins.hasPlugin('kotlin-android')
+    final boolean isKotlinPlatformCommon = project.plugins.hasPlugin('kotlin-platform-common')
+    final boolean isKotlinPlatformJvm = project.plugins.hasPlugin('kotlin-platform-jvm')
+    final boolean isKotlinPlatformJs = project.plugins.hasPlugin('kotlin-platform-js')
+    return isKotlin || isKotlinAndroid || isKotlinPlatformCommon || isKotlinPlatformJvm || isKotlinPlatformJs
   }
 
   private static boolean isJavaProject(final Project project) {
     final boolean isJava = project.plugins.hasPlugin('java')
     final boolean isJavaLibrary = project.plugins.hasPlugin('java-library')
-    return isJava || isJavaLibrary
+    final boolean isJavaGradlePlugin = project.plugins.hasPlugin('java-gradle-plugin')
+    return isJava || isJavaLibrary || isJavaGradlePlugin
   }
 
   private static boolean isAndroidProject(final Project project) {
     final boolean isAndroidLibrary = project.plugins.hasPlugin('com.android.library')
     final boolean isAndroidApp = project.plugins.hasPlugin('com.android.application')
-    return isAndroidLibrary || isAndroidApp
+    final boolean isAndroidTest = project.plugins.hasPlugin('com.android.test')
+    final boolean isAndroidFeature = project.plugins.hasPlugin('com.android.feature')
+    final boolean isAndroidInstantApp = project.plugins.hasPlugin('com.android.instantapp')
+    return isAndroidLibrary || isAndroidApp || isAndroidTest || isAndroidFeature || isAndroidInstantApp
   }
 
   private static boolean shouldIgnore(final Project project, final CodeQualityToolsPluginExtension extension) {
--- src/test/groovy/com/vanniktech/code/quality/tools/CommonCodeQualityToolsTest.groovy
+++ src/test/groovy/com/vanniktech/code/quality/tools/CommonCodeQualityToolsTest.groovy
@@ -7,11 +7,22 @@ import org.junit.Before
 
 abstract class CommonCodeQualityToolsTest {
   Project rootProject
+
   Project javaProject
   Project javaLibraryProject
+  Project javaGradlePluginProject // TODO: use for testing
+
   Project kotlinProject
+  Project kotlinAndroidProject // TODO: use for testing
+  Project kotlinPlatformCommonProject // TODO: use for testing
+  Project kotlinPlatformJvmProject // TODO: use for testing
+  Project kotlinPlatformJsProject // TODO: use for testing
+
   Project androidAppProject
   Project androidLibraryProject
+  Project androidTestProject // TODO: use for testing
+  Project androidFeatureProject // TODO: use for testing
+  Project androidInstantAppProject // TODO: use for testing
 
   Project[] projects
 
@@ -20,36 +31,72 @@ abstract class CommonCodeQualityToolsTest {
 
     javaProject = ProjectBuilder.builder().withName('java').withParent(rootProject).build()
     javaProject.plugins.apply('java')
-    javaProject.repositories {
-      jcenter()
-    }
+    javaProject.repositories { jcenter() } // TODO: is this needed?!
 
     javaLibraryProject = ProjectBuilder.builder().withName('java-library').withParent(rootProject).build()
     javaLibraryProject.plugins.apply('java-library')
-    javaLibraryProject.repositories {
-      jcenter()
-    }
+    javaLibraryProject.repositories { jcenter() } // TODO: is this needed?!
 
+    javaGradlePluginProject = ProjectBuilder.builder().withName('java-library').withParent(rootProject).build()
+    javaGradlePluginProject.plugins.apply('java-gradle-plugin')
+    javaGradlePluginProject.repositories { jcenter() } // TODO: is this needed?!
 
     kotlinProject = ProjectBuilder.builder().withName('kotlin').withParent(rootProject).build()
     kotlinProject.plugins.apply('kotlin')
-    kotlinProject.repositories {
-      jcenter()
-    }
+    kotlinProject.repositories { jcenter() } // TODO: is this needed?!
+
+    kotlinAndroidProject = ProjectBuilder.builder().withName('kotlin-android').withParent(rootProject).build()
+    kotlinAndroidProject.plugins.apply('kotlin-android')
+    kotlinAndroidProject.repositories { jcenter() } // TODO: is this needed?!
+
+    kotlinPlatformCommonProject = ProjectBuilder.builder().withName('kotlin-platform-common').withParent(rootProject).build()
+    kotlinPlatformCommonProject.plugins.apply('kotlin-platform-common')
+    kotlinPlatformCommonProject.repositories { jcenter() } // TODO: is this needed?!
+
+    kotlinPlatformJvmProject = ProjectBuilder.builder().withName('kotlin-platform-jvm').withParent(rootProject).build()
+    kotlinPlatformJvmProject.plugins.apply('kotlin-platform-jvm')
+    kotlinPlatformJvmProject.repositories { jcenter() } // TODO: is this needed?!
+
+    kotlinPlatformJsProject = ProjectBuilder.builder().withName('kotlin-platform-js').withParent(rootProject).build()
+    kotlinPlatformJsProject.plugins.apply('kotlin-platform-js')
+    kotlinPlatformJsProject.repositories { jcenter() } // TODO: is this needed?!
 
     androidAppProject = ProjectBuilder.builder().withName('android app').build()
     androidAppProject.plugins.apply('com.android.application')
-    androidAppProject.repositories {
-      jcenter()
-    }
+    androidAppProject.repositories { jcenter() } // TODO: is this needed?!
 
     androidLibraryProject = ProjectBuilder.builder().withName('android library').build()
     androidLibraryProject.plugins.apply('com.android.library')
-    androidLibraryProject.repositories {
-      jcenter()
-    }
+    androidLibraryProject.repositories { jcenter() } // TODO: is this needed?!
 
-    projects = [javaProject, javaLibraryProject, kotlinProject, androidAppProject, androidLibraryProject]
+    androidTestProject = ProjectBuilder.builder().withName('android test').build()
+    androidTestProject.plugins.apply('com.android.test')
+    androidTestProject.repositories { jcenter() } // TODO: is this needed?!
+
+    androidFeatureProject = ProjectBuilder.builder().withName('android feature').build()
+    androidFeatureProject.plugins.apply('com.android.feature')
+    androidFeatureProject.repositories { jcenter() } // TODO: is this needed?!
+
+    androidInstantAppProject = ProjectBuilder.builder().withName('android instant app').build()
+    androidInstantAppProject.plugins.apply('com.android.instantapp')
+    androidInstantAppProject.repositories { jcenter() } // TODO: is this needed?!
+
+    projects = [
+        rootProject,
+        javaProject,
+        javaLibraryProject,
+        javaGradlePluginProject,
+        kotlinProject,
+        kotlinAndroidProject,
+        kotlinPlatformCommonProject,
+        kotlinPlatformJvmProject,
+        kotlinPlatformJsProject,
+        androidAppProject,
+        androidLibraryProject,
+        androidTestProject,
+        androidFeatureProject,
+        androidInstantAppProject
+    ]
   }
 
   static boolean taskDependsOn(final Task task, final String taskName) {
